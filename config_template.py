ea17d640dbe1c7e7dbeb06996261641e464336ae
Martijn van Exel <m@rtijn.org>
2015-11-08 10:21:52 -0700

    template update

--- config_template.py
+++ config_template.py
@@ -1,16 +1,16 @@
 import os
 
+enrollment_source = {
+	'url': '',
+	'class_of_interest': 0
+}
+
 path = {
 	'previous_results': os.path.join(os.path.dirname(os.path.realpath(__file__)), 'previous.json')
 }
 
 email = {
-	'tos': [], # list of email addresses
-	'subject': 'enrollment update for {}'.format(class_of_interest),
-	'body_template', 'Your enrollment has changed for class {class_number}. It was {previous} and now it is {current}!'
-}
-
-enrollment_source = {
-	'url': '', # user import.io to create an api based on a class schedule web page
-	'class_of_interest': 17574
+	'tos': [],
+	'subject': '',
+	'body_template': ''
 }
